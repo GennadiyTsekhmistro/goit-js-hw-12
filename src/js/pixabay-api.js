ce1ab22314e19029e3a8f9c31d5dbc0525d9d221
Gennadiy Tsekhmistro <Tsekhmistro.gennadiy@gmail.com>
2026-04-21 18:49:35 +0300

    add pixabay api

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const BASE_URL = "https://pixabay.com/api/";
+const API_KEY = "ТУТ_ТВІЙ_KEY";
+
+export async function fetchImages(query, page = 1) {
+  const response = await axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: "photo",
+      orientation: "horizontal",
+      safesearch: true,
+      page: page,
+      per_page: 15,
+    },
+  });
+
+  return response.data;
+}
